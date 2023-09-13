aa9249936f18034a2917916222579548789e9b63
abdul2946 <aazim3510@gmail.com>
2023-09-13 18:16:53 +0530

    Third Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,7 @@
+// Mini Cart Scripts
 let openCart = document.querySelector(".cart-button");
 let container = document.querySelector(".container");
-let closeCart = document.querySelector(".close-cart")
-let burgerButton = document.querySelector(".burger-button")
-let closeNav = document.querySelector(".close-nav")
-
+let closeCart = document.querySelector(".close-cart");
 
 openCart.addEventListener("click", () => {
     container.classList.add("active-cart");
@@ -12,6 +10,11 @@ openCart.addEventListener("click", () => {
 closeCart.addEventListener("click", () => {
     container.classList.remove("active-cart")
 })
+
+
+// Mobile Navbar Scripts
+let burgerButton = document.querySelector(".burger-button");
+let closeNav = document.querySelector(".close-nav");
 
 burgerButton.addEventListener("click", () => {
     container.classList.add("active-nav")
